6aa50f512a80369b9e74617bf26f6037d5588861
robineco <robin.schlund@tutanota.de>
2021-11-15 16:53:49 +0100

    init message-service

---
+++ twitta-backend-message-service/src/main/java/de/robinscloud/twittabackendmessageservice/TwittaBackendMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package de.robinscloud.twittabackendmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittaBackendMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittaBackendMessageServiceApplication.class, args);
+	}
+
+}
---
+++ twitta-backend-message-service/src/test/java/de/robinscloud/twittabackendmessageservice/TwittaBackendMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.robinscloud.twittabackendmessageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittaBackendMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
